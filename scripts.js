78263cf2a02afc1d84152ea23c9cd0dd363fa209
Israel-Felipe <israelfelipet@gmail.com>
2022-06-10 10:57:47 -0300

    feito js da selecao dos itens

---
+++ scripts.js
@@ -0,0 +1,75 @@
+/* SELECAO DAS COMIDAS */
+function comida1() {
+    const selecionar = document.querySelector(".comida1");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function comida2() {
+    const selecionar = document.querySelector(".comida2");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function comida3() {
+    const selecionar = document.querySelector(".comida3");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function comida4() {
+    const selecionar = document.querySelector(".comida4");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  /* SELECAO DAS BEBIDAS */
+function bebida1() {
+    const selecionar = document.querySelector(".bebida1");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function bebida2() {
+    const selecionar = document.querySelector(".bebida2");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function bebida3() {
+    const selecionar = document.querySelector(".bebida3");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function bebida4() {
+    const selecionar = document.querySelector(".bebida4");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  /* SELECAO DAS SOBREMESAS */
+function sobremesa1() {
+    const selecionar = document.querySelector(".sobremesa1");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function sobremesa2() {
+    const selecionar = document.querySelector(".sobremesa2");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function sobremesa3() {
+    const selecionar = document.querySelector(".sobremesa3");
+
+    selecionar.classList.toggle("selecao");
+  }
+
+  function sobremesa4() {
+    const selecionar = document.querySelector(".sobremesa4");
+
+    selecionar.classList.toggle("selecao");
+  }
+
