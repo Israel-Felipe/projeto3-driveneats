c96653991d00543c4a466c7c13939dbd13785278
Israel-Felipe <israelfelipet@gmail.com>
2022-06-10 14:20:24 -0300

    apenas um selecionado por lista

--- scripts.js
+++ scripts.js
@@ -1,75 +1,152 @@
 /* SELECAO DAS COMIDAS */
 function comida1() {
-    const selecionar = document.querySelector(".comida1");
+    const comida1 = document.querySelector(".comida1");
+    comida1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const comida2 = document.querySelector(".comida2");
+    comida2.classList.remove("selecao");
+    const comida3 = document.querySelector(".comida3");
+    comida3.classList.remove("selecao");
+    const comida4 = document.querySelector(".comida4");
+    comida4.classList.remove("selecao");
   }
 
   function comida2() {
-    const selecionar = document.querySelector(".comida2");
+    const comida1 = document.querySelector(".comida2");
+    comida1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const comida2 = document.querySelector(".comida1");
+    comida2.classList.remove("selecao");
+    const comida3 = document.querySelector(".comida3");
+    comida3.classList.remove("selecao");
+    const comida4 = document.querySelector(".comida4");
+    comida4.classList.remove("selecao");
   }
+  
 
   function comida3() {
-    const selecionar = document.querySelector(".comida3");
+    const comida1 = document.querySelector(".comida3");
+    comida1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const comida2 = document.querySelector(".comida1");
+    comida2.classList.remove("selecao");
+    const comida3 = document.querySelector(".comida2");
+    comida3.classList.remove("selecao");
+    const comida4 = document.querySelector(".comida4");
+    comida4.classList.remove("selecao");
   }
 
   function comida4() {
-    const selecionar = document.querySelector(".comida4");
+    const comida1 = document.querySelector(".comida4");
+    comida1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const comida2 = document.querySelector(".comida1");
+    comida2.classList.remove("selecao");
+    const comida3 = document.querySelector(".comida2");
+    comida3.classList.remove("selecao");
+    const comida4 = document.querySelector(".comida3");
+    comida4.classList.remove("selecao");
   }
 
   /* SELECAO DAS BEBIDAS */
-function bebida1() {
-    const selecionar = document.querySelector(".bebida1");
+  function bebida1() {
+    const bebida1 = document.querySelector(".bebida1");
+    bebida1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const bebida2 = document.querySelector(".bebida2");
+    bebida2.classList.remove("selecao");
+    const bebida3 = document.querySelector(".bebida3");
+    bebida3.classList.remove("selecao");
+    const bebida4 = document.querySelector(".bebida4");
+    bebida4.classList.remove("selecao");
   }
 
   function bebida2() {
-    const selecionar = document.querySelector(".bebida2");
+    const bebida1 = document.querySelector(".bebida2");
+    bebida1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const bebida2 = document.querySelector(".bebida1");
+    bebida2.classList.remove("selecao");
+    const bebida3 = document.querySelector(".bebida3");
+    bebida3.classList.remove("selecao");
+    const bebida4 = document.querySelector(".bebida4");
+    bebida4.classList.remove("selecao");
   }
+  
 
   function bebida3() {
-    const selecionar = document.querySelector(".bebida3");
+    const bebida1 = document.querySelector(".bebida3");
+    bebida1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const bebida2 = document.querySelector(".bebida1");
+    bebida2.classList.remove("selecao");
+    const bebida3 = document.querySelector(".bebida2");
+    bebida3.classList.remove("selecao");
+    const bebida4 = document.querySelector(".bebida4");
+    bebida4.classList.remove("selecao");
   }
 
   function bebida4() {
-    const selecionar = document.querySelector(".bebida4");
+    const bebida1 = document.querySelector(".bebida4");
+    bebida1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const bebida2 = document.querySelector(".bebida1");
+    bebida2.classList.remove("selecao");
+    const bebida3 = document.querySelector(".bebida2");
+    bebida3.classList.remove("selecao");
+    const bebida4 = document.querySelector(".bebida3");
+    bebida4.classList.remove("selecao");
   }
 
   /* SELECAO DAS SOBREMESAS */
-function sobremesa1() {
-    const selecionar = document.querySelector(".sobremesa1");
+  function sobremesa1() {
+    const sobremesa1 = document.querySelector(".sobremesa1");
+    sobremesa1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const sobremesa2 = document.querySelector(".sobremesa2");
+    sobremesa2.classList.remove("selecao");
+    const sobremesa3 = document.querySelector(".sobremesa3");
+    sobremesa3.classList.remove("selecao");
+    const sobremesa4 = document.querySelector(".sobremesa4");
+    sobremesa4.classList.remove("selecao");
   }
 
   function sobremesa2() {
-    const selecionar = document.querySelector(".sobremesa2");
+    const sobremesa1 = document.querySelector(".sobremesa2");
+    sobremesa1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const sobremesa2 = document.querySelector(".sobremesa1");
+    sobremesa2.classList.remove("selecao");
+    const sobremesa3 = document.querySelector(".sobremesa3");
+    sobremesa3.classList.remove("selecao");
+    const sobremesa4 = document.querySelector(".sobremesa4");
+    sobremesa4.classList.remove("selecao");
   }
+  
 
   function sobremesa3() {
-    const selecionar = document.querySelector(".sobremesa3");
+    const sobremesa1 = document.querySelector(".sobremesa3");
+    sobremesa1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const sobremesa2 = document.querySelector(".sobremesa1");
+    sobremesa2.classList.remove("selecao");
+    const sobremesa3 = document.querySelector(".sobremesa2");
+    sobremesa3.classList.remove("selecao");
+    const sobremesa4 = document.querySelector(".sobremesa4");
+    sobremesa4.classList.remove("selecao");
   }
 
   function sobremesa4() {
-    const selecionar = document.querySelector(".sobremesa4");
+    const sobremesa1 = document.querySelector(".sobremesa4");
+    sobremesa1.classList.toggle("selecao");
 
-    selecionar.classList.toggle("selecao");
+    const sobremesa2 = document.querySelector(".sobremesa1");
+    sobremesa2.classList.remove("selecao");
+    const sobremesa3 = document.querySelector(".sobremesa2");
+    sobremesa3.classList.remove("selecao");
+    const sobremesa4 = document.querySelector(".sobremesa3");
+    sobremesa4.classList.remove("selecao");
   }
 
+
+/* se tantantantan ou trtatantant ou tantanta ou tantantanttnaa $ tantanta ntantantan */
