4ec70cdf3dc0eedc47c4b669a5f64028c82f7368
Israel-Felipe <israelfelipet@gmail.com>
2022-06-13 09:59:14 -0300

    icones funcionando certinho

---
+++ scripts copy.js
@@ -0,0 +1,272 @@
+/* SELECAO DAS COMIDAS */
+function comida1() {
+    const comida1 = document.querySelector(".comida1");
+    comida1.classList.add("selecao");
+    const okcomida1 = document.querySelector(".icone1");
+    okcomida1.classList.remove("escondido");
+
+    const comida2 = document.querySelector(".comida2");
+    comida2.classList.remove("selecao");
+    const okcomida2 = document.querySelector(".icone2");
+    okcomida2.classList.add("escondido");
+
+    const comida3 = document.querySelector(".comida3");
+    comida3.classList.remove("selecao");
+    const okcomida3 = document.querySelector(".icone3");
+    okcomida3.classList.add("escondido");
+
+    const comida4 = document.querySelector(".comida4");
+    comida4.classList.remove("selecao");
+    const okcomida4 = document.querySelector(".icone4");
+    okcomida4.classList.add("escondido");
+  }
+
+  function comida2() {
+    const comida2 = document.querySelector(".comida2");
+    comida2.classList.add("selecao");
+    const okcomida2 = document.querySelector(".icone2");
+    okcomida2.classList.remove("escondido");
+
+    const comida1 = document.querySelector(".comida1");
+    comida1.classList.remove("selecao");
+    const okcomida1 = document.querySelector(".icone1");
+    okcomida1.classList.add("escondido");
+
+    const comida3 = document.querySelector(".comida3");
+    comida3.classList.remove("selecao");
+    const okcomida3 = document.querySelector(".icone3");    
+    okcomida3.classList.add("escondido");
+
+    const comida4 = document.querySelector(".comida4");
+    comida4.classList.remove("selecao");
+    const okcomida4 = document.querySelector(".icone4");
+    okcomida4.classList.add("escondido");
+
+  }
+  
+
+  function comida3() {
+    const comida3 = document.querySelector(".comida3");
+    comida3.classList.add("selecao");
+    const okcomida3 = document.querySelector(".icone3");
+    okcomida3.classList.remove("escondido");
+
+    const comida1 = document.querySelector(".comida1");
+    comida1.classList.remove("selecao");
+    const okcomida1 = document.querySelector(".icone1");
+    okcomida1.classList.add("escondido");
+
+    const comida2 = document.querySelector(".comida2");
+    comida2.classList.remove("selecao");
+    const okcomida2 = document.querySelector(".icone2");    
+    okcomida2.classList.add("escondido");
+
+    const comida4 = document.querySelector(".comida4");
+    comida4.classList.remove("selecao");
+    const okcomida4 = document.querySelector(".icone4");
+    okcomida4.classList.add("escondido");
+  }
+
+  function comida4() {
+    const comida4 = document.querySelector(".comida4");
+    comida4.classList.add("selecao");
+    const okcomida4 = document.querySelector(".icone4");
+    okcomida4.classList.remove("escondido");
+
+    const comida1 = document.querySelector(".comida1");
+    comida1.classList.remove("selecao");
+    const okcomida1 = document.querySelector(".icone1");
+    okcomida1.classList.add("escondido");
+
+    const comida3 = document.querySelector(".comida3");
+    comida3.classList.remove("selecao");
+    const okcomida3 = document.querySelector(".icone3");    
+    okcomida3.classList.add("escondido");
+
+    const comida2 = document.querySelector(".comida2");
+    comida2.classList.remove("selecao");
+    const okcomida2 = document.querySelector(".icone2");
+    okcomida2.classList.add("escondido");
+  }
+
+  /* SELECAO DAS BEBIDAS */
+  function bebida1() {
+    const bebida1 = document.querySelector(".bebida1");
+    bebida1.classList.add("selecao");
+    const okbebida1 = document.querySelector(".icone5");
+    okbebida1.classList.remove("escondido");
+
+    const bebida2 = document.querySelector(".bebida2");
+    bebida2.classList.remove("selecao");
+    const okbebida2 = document.querySelector(".icone6");
+    okbebida2.classList.add("escondido");
+
+    const bebida3 = document.querySelector(".bebida3");
+    bebida3.classList.remove("selecao");
+    const okbebida3 = document.querySelector(".icone7");
+    okbebida3.classList.add("escondido");
+
+    const bebida4 = document.querySelector(".bebida4");
+    bebida4.classList.remove("selecao");
+    const okbebida4 = document.querySelector(".icone8");
+    okbebida4.classList.add("escondido");
+  }
+
+  function bebida2() {
+    const bebida2 = document.querySelector(".bebida2");
+    bebida2.classList.add("selecao");
+    const okbebida2 = document.querySelector(".icone6");
+    okbebida2.classList.remove("escondido");
+
+    const bebida1 = document.querySelector(".bebida1");
+    bebida1.classList.remove("selecao");
+    const okbebida1 = document.querySelector(".icone5");
+    okbebida1.classList.add("escondido");
+
+    const bebida3 = document.querySelector(".bebida3");
+    bebida3.classList.remove("selecao");
+    const okbebida3 = document.querySelector(".icone7");    
+    okbebida3.classList.add("escondido");
+
+    const bebida4 = document.querySelector(".bebida4");
+    bebida4.classList.remove("selecao");
+    const okbebida4 = document.querySelector(".icone8");
+    okbebida4.classList.add("escondido");
+
+  }
+  
+
+  function bebida3() {
+    const bebida3 = document.querySelector(".bebida3");
+    bebida3.classList.add("selecao");
+    const okbebida3 = document.querySelector(".icone7");
+    okbebida3.classList.remove("escondido");
+
+    const bebida1 = document.querySelector(".bebida1");
+    bebida1.classList.remove("selecao");
+    const okbebida1 = document.querySelector(".icone5");
+    okbebida1.classList.add("escondido");
+
+    const bebida2 = document.querySelector(".bebida2");
+    bebida2.classList.remove("selecao");
+    const okbebida2 = document.querySelector(".icone6");    
+    okbebida2.classList.add("escondido");
+
+    const bebida4 = document.querySelector(".bebida4");
+    bebida4.classList.remove("selecao");
+    const okbebida4 = document.querySelector(".icone8");
+    okbebida4.classList.add("escondido");
+  }
+
+  function bebida4() {
+    const bebida4 = document.querySelector(".bebida4");
+    bebida4.classList.add("selecao");
+    const okbebida4 = document.querySelector(".icone8");
+    okbebida4.classList.remove("escondido");
+
+    const bebida1 = document.querySelector(".bebida1");
+    bebida1.classList.remove("selecao");
+    const okbebida1 = document.querySelector(".icone5");
+    okbebida1.classList.add("escondido");
+
+    const bebida3 = document.querySelector(".bebida3");
+    bebida3.classList.remove("selecao");
+    const okbebida3 = document.querySelector(".icone7");    
+    okbebida3.classList.add("escondido");
+
+    const bebida2 = document.querySelector(".bebida2");
+    bebida2.classList.remove("selecao");
+    const okbebida2 = document.querySelector(".icone6");
+    okbebida2.classList.add("escondido");
+  }
+
+  /* SELECAO DAS SOBREMESAS */
+  function sobremesa1() {
+    const sobremesa1 = document.querySelector(".sobremesa1");
+    sobremesa1.classList.add("selecao");
+    const oksobremesa1 = document.querySelector(".icone9");
+    oksobremesa1.classList.remove("escondido");
+
+    const sobremesa2 = document.querySelector(".sobremesa2");
+    sobremesa2.classList.remove("selecao");
+    const oksobremesa2 = document.querySelector(".icone10");
+    oksobremesa2.classList.add("escondido");
+
+    const sobremesa3 = document.querySelector(".sobremesa3");
+    sobremesa3.classList.remove("selecao");
+    const oksobremesa3 = document.querySelector(".icone11");
+    oksobremesa3.classList.add("escondido");
+
+    const sobremesa4 = document.querySelector(".sobremesa4");
+    sobremesa4.classList.remove("selecao");
+    const oksobremesa4 = document.querySelector(".icone12");
+    oksobremesa4.classList.add("escondido");
+  }
+
+  function sobremesa2() {
+    const sobremesa2 = document.querySelector(".sobremesa2");
+    sobremesa2.classList.add("selecao");
+    const oksobremesa2 = document.querySelector(".icone10");
+    oksobremesa2.classList.remove("escondido");
+
+    const sobremesa1 = document.querySelector(".sobremesa1");
+    sobremesa1.classList.remove("selecao");
+    const oksobremesa1 = document.querySelector(".icone9");
+    oksobremesa1.classList.add("escondido");
+
+    const sobremesa3 = document.querySelector(".sobremesa3");
+    sobremesa3.classList.remove("selecao");
+    const oksobremesa3 = document.querySelector(".icone11");    
+    oksobremesa3.classList.add("escondido");
+
+    const sobremesa4 = document.querySelector(".sobremesa4");
+    sobremesa4.classList.remove("selecao");
+    const oksobremesa4 = document.querySelector(".icone12");
+    oksobremesa4.classList.add("escondido");
+
+  }
+  
+
+  function sobremesa3() {
+    const sobremesa3 = document.querySelector(".sobremesa3");
+    sobremesa3.classList.add("selecao");
+    const oksobremesa3 = document.querySelector(".icone11");
+    oksobremesa3.classList.remove("escondido");
+
+    const sobremesa1 = document.querySelector(".sobremesa1");
+    sobremesa1.classList.remove("selecao");
+    const oksobremesa1 = document.querySelector(".icone9");
+    oksobremesa1.classList.add("escondido");
+
+    const sobremesa2 = document.querySelector(".sobremesa2");
+    sobremesa2.classList.remove("selecao");
+    const oksobremesa2 = document.querySelector(".icone10");    
+    oksobremesa2.classList.add("escondido");
+
+    const sobremesa4 = document.querySelector(".sobremesa4");
+    sobremesa4.classList.remove("selecao");
+    const oksobremesa4 = document.querySelector(".icone12");
+    oksobremesa4.classList.add("escondido");
+  }
+
+  function sobremesa4() {
+    const sobremesa4 = document.querySelector(".sobremesa4");
+    sobremesa4.classList.add("selecao");
+    const oksobremesa4 = document.querySelector(".icone12");
+    oksobremesa4.classList.remove("escondido");
+
+    const sobremesa1 = document.querySelector(".sobremesa1");
+    sobremesa1.classList.remove("selecao");
+    const oksobremesa1 = document.querySelector(".icone9");
+    oksobremesa1.classList.add("escondido");
+
+    const sobremesa3 = document.querySelector(".sobremesa3");
+    sobremesa3.classList.remove("selecao");
+    const oksobremesa3 = document.querySelector(".icone11");    
+    oksobremesa3.classList.add("escondido");
+
+    const sobremesa2 = document.querySelector(".sobremesa2");
+    sobremesa2.classList.remove("selecao");
+    const oksobremesa2 = document.querySelector(".icone10");
+    oksobremesa2.classList.add("escondido");
+  }
